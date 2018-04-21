b8fcdf5f818c49e9ef7416c914eebfb58f9fe0ed
Colby Tobin <colby@lawn-128-61-124-251.lawn.gatech.edu>
2018-04-21 17:46:41 -0400

    confirmed and unconfirmed properties for admin page

--- admin/unconfirmed_properties.php
+++ admin/unconfirmed_properties.php
@@ -42,3 +42,199 @@ $properties = $mysqli->query("SELECT * FROM (SELECT Property.Name, Property.Stre
                 WHERE ApprovedBy IS NULL $searchquery $sorttype $sortdirection");
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Confirmed Properties</title>
+
+    <style>
+        #title {
+            text-align:center;
+            padding:40px;
+            color: #edf5e1;
+            text-shadow: 2px 2px #000000;
+            font-family: Open Sans, Arial;
+            font-weight: 300;
+            font-size: 3em;
+
+        }
+
+        #property_table {
+
+            font-family: Open Sans, Arial;
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        #property_table td, #property_table th {
+
+            border: 1px solid #000000;
+            padding: 8px;
+
+        }
+
+        #property_table tr:hover {background-color: #ddd;}
+
+        #property_table th {
+
+            padding-top: 12px;
+            padding-bottom: 12px;
+            text-align: left;
+            background-color: #4CAF50;
+            color: white;
+
+        }
+
+        body {
+            background-color: #5cdb95;
+        }
+
+        .button{
+            width: 200px;
+            background-color: #edf5e1;
+            border: none;
+            color: #000000;
+            padding: 10px 10px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 4px 2px;
+            cursor: pointer;
+        }
+
+
+        div {
+
+            border-radius: 5px;
+            padding: 20px;
+
+        }
+
+        input[type=text] {
+            padding: 10px 10px;
+            margin: 8px 0;
+            display: inline;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+            font-family: Open Sans, Arial;
+            font-weight: 300;
+        }
+
+        .owner_options {
+            width: 200px;
+        }
+
+
+    </style>
+    <script>
+
+        function onSelectChange() {
+            var selectValue = document.getElementById("searchtype").value;
+            if (selectValue == "Size" || selectValue == "Rating" || selectValue == "Visits") {
+                document.getElementById("lowerbound").style.visibility = "visible";
+                document.getElementById("upperbound").style.visibility = "visible";
+                document.getElementById("searchtext").style.visibility = "hidden";
+            } else {
+                document.getElementById("lowerbound").style.visibility = "hidden";
+                document.getElementById("upperbound").style.visibility = "hidden";
+                document.getElementById("searchtext").style.visibility = "visible";
+            }
+        }
+
+        function onSearchPressed() {
+            var selectValue = document.getElementById("searchtype").value;
+            if (selectValue == "Size" || selectValue == "Rating" || selectValue == "Visits") {
+                var lowBound = document.getElementById("lowerbound").value;
+                var upBound = document.getElementById("upperbound").value;
+
+                window.location.replace("unconfirmed_properties.php?searchtype="+selectValue+"&upper="+upBound+"&lower="+lowBound);
+            } else {
+                var searchText = document.getElementById("searchtext").value;
+                window.location.replace("unconfirmed_properties.php?searchtype="+selectValue+"&searchtext="+searchText);
+            }
+        }
+
+    </script>
+</head>
+
+
+<body>
+<h1 id="title"><strong>Confirmed Properties</strong></h1>
+
+
+<table size="100%" id="property_table">
+
+    <tr>
+        <th>Name</th>
+        <th>Address</th>
+        <th>City</th>
+        <th>Zip Code</th>
+        <th>Size</th>
+        <th>Type</th>
+        <th>Public</th>
+        <th>Commercial</th>
+        <th>Approved Username</th>
+        <th>Visits</th>
+        <th>Avg. Rating</th>
+    </tr>
+
+    <?php
+    while ($row = mysqli_fetch_assoc($properties)) {
+        ?>
+        <tr>
+            <th><a href="manage_property.php?property_id=<?php echo $row['ID']; ?>"><?php echo $row['Name']; ?></a></th>
+            <th><?php echo $row['Street']; ?></th>
+            <th><?php echo $row['City']; ?></th>
+            <th><?php echo $row['Zip']; ?></th>
+            <th><?php echo $row['Size']; ?></th>
+            <th><?php echo $row['PropertyType']; ?></th>
+            <th><?php echo $row['IsPublic']; ?></th>
+            <th><?php echo $row['IsCommerical']; ?></th>
+            <th><?php echo $row['ID']; ?></th>
+            <th><?php echo $row['ApprovedBy']; ?></th>
+            <th><?php echo $row['Visits']; ?></th>
+            <th><?php echo $row['Rating']; ?></th>
+        </tr>
+        <?php
+    }
+    ?>
+
+</table>
+<br>
+
+<table width="100%">
+
+    <tr>
+        <td>
+            <select id="searchtype" class="owner_options" onchange="onSelectChange()">
+                <option value="Name">Name</option>
+                <option value="Street">Address</option>
+                <option value="City">City</option>
+                <option value="Zip">Zip</option>
+                <option value="Size">Size</option>
+                <option value="PropertyType">Type</option>
+                <option value="Visits">Visits</option>
+                <option value="Rating">Average Rating</option>
+            </select>
+        </td>
+    </tr>
+    <tr>
+        <td><input type="text" id="searchtext" name="searchtext" placeholder="Search Term" class="owner_options"></td>
+        <td><input type="text" id="lowerbound" name="lowerbound" placeholder="Lower Bound of Search" class="owner_options" style="visibility: hidden;"></td>
+        <td><input type="text" id="upperbound" name="upperbound" placeholder="Upper Bound of Search" class="owner_options" style="visibility: hidden;"></td>
+    </tr>
+    <tr>
+        <td><button type="button" class="button" onclick="onSearchPressed()">Search Properties</button></td>
+        <td><a href="../user/mainpage.php">Back</a></td>
+    </tr>
+
+
+
+</table>
+
+</body>
+</html>
